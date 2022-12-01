563f46c1332827424efec2fb3e6002602870f2ce
Maud Frichement <maudfrichement@MacBook-Air-de-Maud.local>
2022-12-01 22:28:00 +0100

    UI V1

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,26 +5,55 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-const basePromptPrefix =
-`Écrit moi une fable pour enfant à la façon de Jean de La Fontaine. La fable doit contenir une morale inclue à la fin de l'histoire. Les deux personnages sont : 
-`
+
 const generateAction = async (req, res) => {
+  //Get a description of the company requested skills
+  const skillsDescriptionPrompt =
+  `Make a summary of the skills requested by this job offer.
+  Company main mission
+  Job offer:  ${req.body.userInputOffer}
+  `
   // Run first prompt
-  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+  console.log(`API: ${skillsDescriptionPrompt}`)
 
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    prompt: `${skillsDescriptionPrompt}\n`,
     temperature: 0.5,
     max_tokens: 700,
   });
   
   const basePromptOutput = baseCompletion.data.choices.pop();
-  console.log();
+  console.log(basePromptOutput);
 
+  // I build Prompt #2.
+  const offerDetailPrompt =
+  `Write a motivation letter for a startup company that includes the problem the company is solving, the help I can bring to the team and the value I will create. Tell why I am the best candidate, why I'm interested in product management and why I want to work for this company. 
 
-  res.status(200).json({ output: basePromptOutput });
+  Company Name: ${req.body.userInputCompany}
+  Company Main Mission: ${req.body.userInputMission}
+  
+  Job offer Requested skills: ${basePromptOutput}
+  
+  Candidate description: ${req.body.userInputJob}
+  Candidate main skills to highligh: ${req.body.userInputSkills}
+  Candidate main past experiences: ${req.body.userInputExp}
+  `
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${offerDetailPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+		// I also increase max_tokens.
+    max_tokens: 1250,
+  });
+  
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
 
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
 };
 
 export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,96 +1,159 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import rocket from '../assets/rocket.png';
 import { useState } from 'react';
 
 const Home = () => {
-  const [userInput, setUserInput] = useState('');
+  const [userInputJob, setUserInputJob] = useState('');
+  const [userInputSkills, setUserInputSkills] = useState('');
+  const [userInputCompany, setUserInputCompany] = useState('');
+  const [userInputExp, setUserInputExp] = useState('');
+  const [userInputOffer, setUserInputOffer] = useState('');
+  const [userInputMission, setUserInputMission] = useState('');
   const [apiOutput, setApiOutput] = useState('');
   const [isGenerating, setIsGenerating] = useState(false);
 
   const callGenerateEndpoint = async () => {
     setIsGenerating(true);
     
-    console.log("Calling OpenAI...")
+    console.log("Calling OpenAI...");
     const response = await fetch('/api/generate', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ userInput }),
+      body: JSON.stringify({ userInputJob, userInputCompany, userInputOffer, userInputSkills }),
     });
 
     const data = await response.json();
     const { output } = data;
-    console.log("OpenAI replied...", output.text)
+    console.log("OpenAI replied...", output.text);
 
     setApiOutput(`${output.text}`);
     setIsGenerating(false);
-  }
+  };
 
-  const onUserChangedText = (event) => {
-    setUserInput(event.target.value);
+  const onUserChangedJob = (event) => {
+    setUserInputJob(event.target.value);
+    
+  };
+
+  const onUserChangedSkills = (event) => {
+    setUserInputSkills(event.target.value);
+    
+  };
+
+  const onUserChangedCompany = (event) => {
+    setUserInputCompany(event.target.value);
+
+  };
+
+  const onUserChangedOffer = (event) => {
+    setUserInputOffer(event.target.value);
+
+  };
+  const onUserChangedMission = (event) => {
+    setUserInputMission(event.target.value);
+
+  };
+
+  const onUserChangedExp = (event) => {
+    setUserInputExp(event.target.value);
+
   };
 
 
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Your perfect motivation letter</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>Ma fable comme La Fontaine</h1>
+            <h1>Generate your ideal motivation letter</h1>
           </div>
           <div className="header-subtitle">
-            <h2>Choisi deux ou trois animaux et obtient ta fable personnalisée.</h2>
+            <h2>Fill some informations and get your perfect motivation letter in a few seconds.</h2>
           </div>
+          
         </div>
 
         <div className="prompt-container">
-          <textarea
-          className="prompt-box"
-          placeholder="Choisi tes personnages"
-          value={userInput}
-          onChange={onUserChangedText}/>
-        </div>
+          <div className='prompt-group'>
+            <label>Who are you?</label> 
+            <textarea
+            className="prompt-box"
+            placeholder="Enter your main job and years of experience"
+            value={userInputJob}
+            onChange={onUserChangedJob}/>
+            
+            <textarea
+            className="prompt-box"
+            placeholder="Your main skills"
+            value={userInputSkills}
+            onChange={onUserChangedSkills}/>
+            
+            <textarea
+            className="prompt-box"
+            placeholder="Your mains past experiences related to the job"
+            value={userInputExp}
+            onChange={onUserChangedExp}/>
+          </div>
+          
+          <div className='prompt-group'>
+            <label>What is the job you want?</label> 
+            <textarea
+            className="prompt-box"
+            placeholder="The name of the company you apply for"
+            value={userInputCompany}
+            onChange={onUserChangedCompany}/>
+            
+            <textarea
+            className="prompt-box"
+            placeholder="The company main mission"
+            value={userInputMission}
+            onChange={onUserChangedMission}/>
 
-        <div className="prompt-buttons">
-          <a className={isGenerating ? 'generate-button loading' : 'generate-button'}
-            onClick={callGenerateEndpoint}
-          >
-            <div className="generate">
-            {isGenerating ? <span className="loader"></span> : <p>Créer ma fable</p>}
-            </div>
-          </a>
-        </div>
+            <textarea
+            className="prompt-box"
+            placeholder="The skills requested by the company for this job offer"
+            value={userInputOffer}
+            onChange={onUserChangedOffer}/>
+            
+          </div>
 
-        {apiOutput && (
+          <div className="prompt-buttons">
+            <a className={isGenerating ? 'generate-button loading' : 'generate-button'}
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+              {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+          </div>
+
+
+          {apiOutput && (
           <div className="output">
             <div className="output-header-container">
               <div className="output-header">
-                <h3>Votre fable</h3>
+                <h3>Your letter</h3>
               </div>
             </div>
             <div className="output-content">
               {isGenerating ? <span className="loader"></span> : <p>{apiOutput}</p>}
             </div>
           </div>
-        )}
+          )}
+
+
+          
+        </div>
+
       </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
-      </div>
+
     </div>
   );
 };
